3f3655ae99a2ad719af86aa2cf4500ad30fe852e
2531251963 <2531251963@qq.com>
2019-09-01 16:19:42 +0800

    f

--- src/main/java/org/easyarch/configcenter/ConfigCenterManager.java
+++ src/main/java/org/easyarch/configcenter/ConfigCenterManager.java
@@ -6,8 +6,10 @@ import org.easyarch.configcenter.annotation.ConfigValue;
 
 import java.io.File;
 import java.io.FileFilter;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.util.Enumeration;
@@ -15,6 +17,8 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * @ClassName ConfigCenterManager
@@ -104,8 +108,9 @@ public class ConfigCenterManager {
         // 第一个class类的集合
         // 是否循环迭代
         boolean recursive = true;
+        String packageName = pack;
         // 获取包的名字 并进行替换
-        String packageDirName = pack.replace('.', '/');
+        String packageDirName = packageName.replace('.', '/');
         // 定义一个枚举的集合 并进行循环来处理这个目录下的things
         Enumeration<URL> dirs;
 
@@ -125,9 +130,62 @@ public class ConfigCenterManager {
                     String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
                     // 以文件的方式扫描整个包下的文件 并添加到集合中
                     findAndAddClassesInPackageByFile(pack, filePath, recursive, classes_set);
+                } else if ("jar".equals(protocol)) {
+                    // 如果是jar包文件
+                    // 定义一个JarFile
+                    JarFile jar;
+                    try {
+                        // 获取jar
+                        jar = ((JarURLConnection) url.openConnection())
+                                .getJarFile();
+                        // 从此jar包 得到一个枚举类
+                        Enumeration<JarEntry> entries = jar.entries();
+                        // 同样的进行循环迭代
+                        while (entries.hasMoreElements()) {
+                            // 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
+                            JarEntry entry = entries.nextElement();
+                            String name = entry.getName();
+                            // 如果是以/开头的
+                            if (name.charAt(0) == '/') {
+                                // 获取后面的字符串
+                                name = name.substring(1);
+                            }
+                            // 如果前半部分和定义的包名相同
+                            if (name.startsWith(packageDirName)) {
+                                int idx = name.lastIndexOf('/');
+                                // 如果以"/"结尾 是一个包
+                                if (idx != -1) {
+                                    // 获取包名 把"/"替换成"."
+                                    packageName = name.substring(0, idx)
+                                            .replace('/', '.');
+                                }
+                                // 如果可以迭代下去 并且是一个包
+                                if ((idx != -1) || recursive) {
+                                    // 如果是一个.class文件 而且不是目录
+                                    if (name.endsWith(".class")
+                                            && !entry.isDirectory()) {
+                                        // 去掉后面的".class" 获取真正的类名
+                                        String className = name.substring(
+                                                packageName.length() + 1, name
+                                                        .length() - 6);
+                                        try {
+                                            // 添加到classes
+                                            classes_set.add(Class
+                                                    .forName(packageName + '.'
+                                                            + className));
+                                        } catch (ClassNotFoundException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    } catch (IOException e) {
+                        // log.error("在扫描用户定义视图时从jar包获取文件出错");
+                        e.printStackTrace();
+                    }
                 }
             }
-
         return classes_set;
     }
 
@@ -170,6 +228,7 @@ public class ConfigCenterManager {
             }
         }
     }
+
     private void getAllConfig(Set<Class<?>> classes) throws Exception {
         for (Class<?> clazz :
                 classes) {
